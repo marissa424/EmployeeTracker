88d3dc65cae0bb9b914e692f4b894cc9bf00b3e8
Marissa T <marissalynnthompson@gmail.com>
2023-05-08 17:53:03 -0400

    updated and fixed index.js code

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection({
   host: 'localhost',
-  port: 3001,
+  port: 3306,
   user: 'root',
   password: 'Algorithms21',
   database: 'employees'
